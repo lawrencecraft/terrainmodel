0c794894936f1b3c8a287b6ae3ebcf82db6d4348
Lawrence Craft <lawrence.craft@gmail.com>
2015-06-12 20:03:55 +0100

    Fixes #1. Reverting single line layout

--- terrain.go
+++ terrain.go
@@ -4,18 +4,22 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"io"
 )
 
 type Terrain struct {
-	layout    []uint16
+	layout    [][]uint16
 	X         uint16
 	Y         uint16
 	MaxHeight uint16
 }
 
 func New(x uint16, y uint16, max_height uint16) *Terrain {
-	t := Terrain{X: x, Y: y, layout: make([]uint16, int64(x)*int64(y)), MaxHeight: max_height}
+	t := Terrain{X: x, Y: y, layout: make([][]uint16, int64(x)), MaxHeight: max_height}
+	for i := range t.layout {
+		t.layout[i] = make([]uint16, int64(y))
+	}
 	return &t
 }
 
@@ -28,18 +32,16 @@ func (t *Terrain) GetHeight(x uint16, y uint16) (num uint16, err error) {
 	if x > t.X-1 || y > t.Y-1 {
 		return 0, errors.New("Index out of range")
 	}
-	index := t.getIndex(x, y)
 
-	return t.layout[index], nil
+	return t.layout[x][y], nil
 }
 
 func (t *Terrain) SetHeight(x uint16, y uint16, height uint16) (err error) {
 	if x > t.X-1 || y > t.Y-1 {
 		return errors.New(fmt.Sprintf("%d,%d is out of range (max %d,%d)", x, y, t.X, t.Y))
 	}
-	index := t.getIndex(x, y)
 
-	t.layout[index] = height
+	t.layout[x][y] = height
 
 	return nil
 }
@@ -59,10 +61,10 @@ func (t *Terrain) CopyTo(destination *Terrain, xOffset uint16, yOffset uint16) e
 		for y := uint16(0); y < destination.Y; y++ {
 			sourceX := x + xOffset
 			sourceY := y + yOffset
-			sourceIndex := t.getIndex(sourceX, sourceY)
-			destIndex := destination.getIndex(x, y)
 
-			destination.layout[destIndex] = t.layout[sourceIndex]
+			log.Debug("Setting ", sourceX, ",", sourceY, " to ", x, ",", y)
+
+			destination.layout[x][y] = t.layout[sourceX][sourceY]
 		}
 	}
 
@@ -72,7 +74,7 @@ func (t *Terrain) CopyTo(destination *Terrain, xOffset uint16, yOffset uint16) e
 func (t *Terrain) Iterate(fn func(uint16, uint16, uint16)) {
 	for x := uint16(0); x < t.X; x++ {
 		for y := uint16(0); y < t.Y; y++ {
-			fn(x, y, t.layout[t.getIndex(x, y)])
+			fn(x, y, t.layout[x][y])
 		}
 	}
 }
